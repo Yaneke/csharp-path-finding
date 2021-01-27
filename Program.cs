70e1b2077789aa95a73790cb6e0f80886d6182aa
Yannick Molinghen <yannick.molinghen@ulb.be>
2021-01-27 12:35:23 +0100

    Added Heap/PriorityQueue benchmarks

--- Program.cs
+++ Program.cs
@@ -1,9 +1,22 @@
-﻿using visualisation;
+﻿using System;
+using visualisation;
+using Benchmarks;
+using BenchmarkDotNet.Running;
 
 namespace pathfinding {
     class Program {
         static void Main(string[] args) {
-            RunServer();
+            if (args.Length == 0 || args[0] == "runserver") {
+                RunServer();
+            } else if (args[0] == "benchmark") {
+                var res1 = BenchmarkRunner.Run<PriorityQueueBenchmarks>();
+                var res2 = BenchmarkRunner.Run<HeapBenchmarks>();
+                Console.WriteLine(res1);
+                Console.WriteLine(res2);
+
+            } else {
+                Console.WriteLine("Unknown command! Use \"runserver\" or \"benchmark\"");
+            }
         }
 
         static void RunServer() {
